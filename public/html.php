97741e3e3384fc10d093742b68150c96ec9e1d74
olegs.zizins@1clickgames.com <olegs.zizins@1clickgames.com>
2018-04-18 14:11:09 +0300

    Added testing case

--- public/html.php
+++ public/html.php
@@ -9,3 +9,36 @@ while ( $a < 1000)
     echo "Var ar = $a";
     $a ++;
 }
+
+
+while ( $a < 1000)
+{
+    echo "Var ar = $a";
+    $a ++;
+}
+
+
+while ( $a < 1000)
+{
+    echo "Var ar = $a";
+    $a ++;
+}
+
+
+
+while ( $a < 1000)
+{
+    echo "Var ar = $a";
+    $a ++;
+}
+
+
+
+while ( $a < 1000)
+{
+    echo "Var ar = $a";
+    $a ++;
+}
+
+
+echo "End for testing!!!!";
